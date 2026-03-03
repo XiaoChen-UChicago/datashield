5479f9c8939c977a06dc6b867eb889ebc9dba81a
XiaoChen-UChicago <chenxiao@uchicago.edu>
2026-03-03 12:12:25 -0600

    fix duplicate dynamic export

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+
+
 import { Workspace } from "@/components/workspace";
 import { prisma } from "@/lib/prisma";
 import { DetectionReportItem } from "@/components/workspace";
